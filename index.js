5440d55b684c454d8883f0a60276b1791c5787ac
fardog <nate@milkandtang.com>
2015-04-21 11:20:13 -0700

    Fix style and add copyright header

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
+/**
+ * Copyright 2015 Urban Airship and Contributors
+ */
 var parser = require('tap-parser')
 
-var p = parser(function(results){
+var p = parser(function(results) {
   process.exit(Number(!results.ok))
-});
+})
 
-p.on('assert', function(assert){
-  if (!assert.ok) process.exit(1)
+p.on('assert', function(assert) {
+  if(!assert.ok) {
+    process.exit(1)
+  }
 })
 
 process.stdin.pipe(process.stdout)
